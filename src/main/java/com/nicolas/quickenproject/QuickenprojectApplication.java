bbbc5152eeabec4076172c73678c5f6d3a30b497
Nicolas Beringer <nicolas94010@gmail.com>
2025-12-05 11:01:48 -0800

    Initial project setup

---
+++ src/main/java/com/nicolas/quickenproject/QuickenprojectApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.quickenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickenprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickenprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolas/quickenproject/QuickenprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.quickenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickenprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
